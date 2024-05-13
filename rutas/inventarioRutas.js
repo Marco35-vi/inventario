22261cfde244b1c11485b0c15b6308670309574d
Marco35-vi <147777568+Marco35-vi@users.noreply.github.com>
2024-05-12 21:21:09 -0400

    anexion endpoint de creacion  modificacion eliminacion

--- rutas/inventarioRutas.js
+++ rutas/inventarioRutas.js
@@ -11,4 +11,44 @@ rutas.get('/traerInventario', async (req, res) => {
         res.status(500).json({mensaje: error.mensaje});
     }
 });
+//endpoint 2. Añadir
+rutas.post('/agregar', async (req, res) => {
+    const inventario = new InventarioModel({
+        nombreproducto: req.body.nombreproducto,
+        descripcion: req.body.descripcion,
+        cantidades: req.body.cantidades
+    })
+    try {
+        const nuevoInventario = await inventario.save();
+        res.status(201).json(nuevoInventario);
+    } catch (error) {
+        res.status(400).json({ mensaje :  error.message})
+    }
+});
+//endpoint 3. Editar
+rutas.put('/editar/:id', async (req, res) => {
+    try {
+        const inventarioEditado = await InventarioModel.findByIdAndUpdate(req.params.id, req.body, { new : true });
+        if (!inventarioEditado)
+            return res.status(404).json({ mensaje : 'Producto no encontrado!!!'});
+        else
+            return res.status(201).json(inventarioEditado);
+
+    } catch (error) {
+        res.status(400).json({ mensaje :  error.message})
+    }
+})
+//ENDPOINT 4. eliminar
+rutas.delete('/eliminar/:id',async (req, res) => {
+    try {
+       const productoEliminado = await InventarioModel.findByIdAndDelete(req.params.id);
+       if (!productoEliminado)
+            return res.status(404).json({ mensaje : 'producto no encontrado!!!'});
+       else 
+            return res.json({mensaje :  'producto eliminado'});    
+       } 
+    catch (error) {
+        res.status(500).json({ mensaje :  error.message})
+    }
+});
 module.exports = rutas;
